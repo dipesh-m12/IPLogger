8456aba4d75e94c82430f7521b504882b419dff0
dipesh-m12 <mavinash423@gmail.com>
2024-09-01 01:26:14 +0530

    ui

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://mavinash422:gfcz0io0dke50Xmb@cluster0.cj5pp.mongodb.net/analysisData?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Db Connected");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+const schema = mongoose.Schema({
+  userId: {
+    type: String,
+  },
+  ip: {
+    type: String,
+  },
+});
+const model = mongoose.model("ips", schema);
+
+const getClientIp = (req) => {
+  return (
+    req.ip || req.connection.remoteAddress || req.headers["x-forwarded-for"]
+  );
+};
+
+app.get("/", async (req, res) => {
+  const userId = req.query.userId;
+  const ip = getClientIp(req);
+  console.log(typeof userId);
+
+  await model.create({ userId: userId.toString(), ip });
+  console.log(`User ID: ${userId}, IP: ${ip}`);
+
+  res.send("Success!");
+});
+
+app.listen(5001, () => console.log("Server listening on port 3000"));
